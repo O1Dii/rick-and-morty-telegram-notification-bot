25a1c9c2c4e1df4b526c9e2963466f61cc6c5818
O1Dii <oldi730051@mail.ru>
2019-11-15 12:38:42 +0300

    Changed dynamodb initialization

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,14 @@ app = Flask(__name__)
 
 BOT_TOKEN = os.getenv('TOKEN')
 TABLE_NAME = os.getenv('TABLE_NAME')
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 BASE_URL = f'https://api.telegram.org/bot{BOT_TOKEN}/'
 
-dynamodb = boto3.resource('dynamodb', region_name='eu-central-1')
+dynamodb = boto3.resource('dynamodb',
+                          region_name='eu-central-1',
+                          aws_access_key_id=AWS_ACCESS_KEY_ID,
+                          aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 table = dynamodb.Table(TABLE_NAME)
 
 from app.routes import *
