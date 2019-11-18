4b68b80994cf98e8e2ab6bde38ab1b7c49e9fb41
O1Dii <oldi730051@mail.ru>
2019-11-18 10:51:45 +0300

    Fixed 'when' command

--- app/telegram_commands.py
+++ app/telegram_commands.py
@@ -40,10 +40,19 @@ def handle_command(message):
         delete_from_db(chat_id)
 
     if text == '/when':
-        days_left = 7 - date.today().weekday()
+        days_left = 6 - date.today().weekday()
 
         if days_left:
-            result_string = f'дней до выхода новой серии - {days_left}'
+            result_string = 'до выхода новой серии '
+
+            if days_left == 1:
+                result_string += f'остался 1 день'
+            if 1 < days_left <= 4:
+                result_string += f'осталось {days_left} дня'
+            if days_left > 4:
+                result_string += f'осталось {days_left} дней'
+
+            result_string += ', озвучка Сыендука выйдет вечером следующего дня'
         else:
             result_string = 'Новая серия выходит сегодня'
 
